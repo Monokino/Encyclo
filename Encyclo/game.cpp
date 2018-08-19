69883b3277d9b85f4b666c34339cac897371add6
Volodymyr Stefaniak <vstefan@softserveinc.com>
2018-08-19 11:10:54 +0300

    Revert "small changes"
    This reverts commit 4aeec7b883b2afac85fd4dd2828640179f4ec2b5.

--- Encyclo/game.cpp
+++ Encyclo/game.cpp
@@ -9,14 +9,14 @@
 
 Game::Game(QWidget* parent) : QMainWindow(parent)
 {
-    this->pagesWidget_ = new QStackedWidget();
+    this->pagesWidget_ = new QStackedWidget;
     this->setCentralWidget(pagesWidget_);
     this->setFixedSize(400, 600);
 
-    pagesWidget_->addWidget(new EncycloWidget(pagesWidget_, this));
-    pagesWidget_->addWidget(new NewGameWidget(pagesWidget_, this));
-    pagesWidget_->addWidget(new LoginUserWidget(pagesWidget_, this));
-    pagesWidget_->addWidget(new AccountSettingsWidget(pagesWidget_, this));
+    pagesWidget_->addWidget(new EncycloWidget(pagesWidget_));
+    pagesWidget_->addWidget(new NewGameWidget(pagesWidget_));
+    pagesWidget_->addWidget(new LoginUserWidget(pagesWidget_));
+    pagesWidget_->addWidget(new AccountSettingsWidget(pagesWidget_));
 
     pagesWidget_->setCurrentIndex(static_cast<ushort>(WidgetType::LoginUserWidget));
 }
